decbe8c4afd7be3c996aa370472fe43022c71083
yuruohan <1005623825@qq.com>
2021-05-27 15:53:34 +0800

    添加主页测试入口

--- src/components/login_success.vue
+++ src/components/login_success.vue
@@ -7,6 +7,12 @@
           <el-button type="primary" @click='clickpersonal'>个人中心</el-button>
           <el-button type="primary" @click='clicksearch'>搜索</el-button>
           <el-button type="primary" @click='clickforum'>论坛</el-button>
+          <br></br>
+          <el-button-group>
+            <el-button type="primary" @click='clickdoctor'>医生主页</el-button>
+            <el-button type="primary" @click='clickdepartment'>科室主页</el-button>
+            <el-button type="primary" @click='clickhospital'>医院主页</el-button>
+          </el-button-group>
         </div>
       </div>
     </div>
@@ -25,6 +31,31 @@ export default {
     },
     clickforum () {
       console.log('Forum!')
+    },
+
+    clickdoctor () {
+      this.$router.push({
+          name: 'DoctorHomepage',
+          params: {
+            doctorName: "test_doctor_name",
+          }
+        })
+    },
+    clickdepartment () {
+      this.$router.push({
+          name: 'DepartmentHomepage',
+          params: {
+            departmentName: "test_dept_name",
+          }
+        })
+    },
+    clickhospital () {
+      this.$router.push({
+          name: 'HospitalHomepage',
+          params: {
+            hospitalName: "test_hospital_name",
+          }
+        })
     }
   }
 }
@@ -47,8 +78,8 @@ export default {
   }
   .where_to_btn_back{
     margin-top: 66px;
-    margin-left: 40%;
-    margin-right: 40%;
+    margin-left: 25%;
+    margin-right: 25%;
     padding-top: 25px;
     padding-bottom: 25px;
     background-color: #ffffff66;
